7127272b5daee1a9efff41545b0a1d2831cf20f3
Carlos <carlosaugusto@bodyfitteam.com.br>
2023-04-13 23:07:29 -0300

    removendo

--- pages/tags.tsx
+++ pages/tags.tsx
@@ -1,9 +1,6 @@
-import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
-import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllTags } from '@/lib/tags'
-import kebabCase from '@/lib/utils/kebabCase'
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
 
 export const getStaticProps: GetStaticProps<{ tags: Record<string, number> }> = async () => {
@@ -13,9 +10,8 @@ export const getStaticProps: GetStaticProps<{ tags: Record<string, number> }> = 
 }
 
 export default function Tags({ tags }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const sortedTags = Object.keys(tags).sort((a, b) => tags[b] - tags[a])
   return (
-    <>teste
+    <>
       <PageSEO title={`Tags - ${siteMetadata.author}`} description="Things I blog about" />
       <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
         <div className="space-x-2 pb-8 pt-6 md:space-y-5">
