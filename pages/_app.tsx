ab2d724499ff29ae89adf1ec2cb83fa78eb9edec
Carlos <carlosaugusto@bodyfitteam.com.br>
2023-04-14 21:36:21 -0300

    Resolvido o problema de thema

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,14 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
-
 import '@fontsource/inter/variable-full.css'
-
-import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-
 import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
+import { ThemeProvider } from 'next-themes'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
